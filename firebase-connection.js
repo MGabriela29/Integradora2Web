62c9d212c1dd23adbbba7c00ecb18b5e86338783
Gabriela <gabsram2975@gmail.com>
2024-04-09 19:55:20 -0600

    04 Funcionalidad del sitio.. LISTO

--- firebase-connection.js
+++ firebase-connection.js
@@ -1,56 +1,49 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js';
-import { getFirestore, collection, query, where, getDocs } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js';
+import { getFirestore, collection, query, getDocs } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js';
 
 // Firebase configuration object
 const firebaseConfig = {
-    apiKey: "AIzaSyDflVrzbmpcWrOyOotE1ltX6RQ5hil9MaA",
-    authDomain: "pruebacoments.firebaseapp.com",
-    databaseURL: "https://pruebacoments-default-rtdb.firebaseio.com",
-    projectId: "pruebacoments",
-    storageBucket: "pruebacoments.appspot.com",
-    messagingSenderId: "78431417841",
-    appId: "1:78431417841:web:72e6c954389b7d5e5c0050"
+    apiKey: "AIzaSyDrZm4mfMAyRkmwPI0BQ7U5Ycaqbygv20A",
+    authDomain: "smiccat-5d93b.firebaseapp.com",
+    databaseURL: "https://smiccat-5d93b-default-rtdb.firebaseio.com",
+    projectId: "smiccat-5d93b",
+    storageBucket: "smiccat-5d93b.appspot.com",
+    messagingSenderId: "306397029142",
+    appId: "1:306397029142:web:998d7786e1311caff4e637",
+    measurementId: "G-6XG0EDYRYW"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Your Firebase database reference
 const db = getFirestore(app);
 
-
-const q = query(collection(db, "comentarios"));
-
-const querySnapshot = await getDocs(q);
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-});
-
-// const comentariosRef = db.ref('comentarios');
-
-// Add Firebase event listeners and update comments dynamically
-comentariosRef.on('child_added', (snapshot) => {
-    const comentario = snapshot.val();
+async function loadComments() {
+    const q = query(collection(db, "comentarios"));
+    const querySnapshot = await getDocs(q);
     const comentariosList = document.getElementById('comments-list');
-    const nuevoComentario = document.createElement('li');
-    nuevoComentario.innerHTML = `
-<div>
-  <div class="comment-avatar"><img src="/istatic/img/avatar.jpg" alt="Foto perfil"></div>
-  <div class="comment-box">
-    <div class="comment-head">
-      <h6 class="comment-name">${comentario.usuario}</h6>
-      <span>${comentario.Fecha}</span>
-      <i class="fa fa-reply"></i>
-      <i class="fa fa-heart"></i>
-    </div>
-    <div class="comment-content">
-      ${comentario.comentario}
-    </div>
-  </div>
-</div>
-`;
-    comentariosList.appendChild(nuevoComentario);
-});
+    querySnapshot.forEach((doc) => {
+        const comentario = doc.data();
+        const fechaActual = new Date().toLocaleDateString();
+        const nuevoComentario = document.createElement('li');
+        nuevoComentario.innerHTML = `
+            <div>
+                <div class="comment-avatar"><img src="/istatic/img/usuario.png" alt="Foto perfil"></div>
+                <div class="comment-box">
+                    <div class="comment-head">
+                        <h6 class="comment-name">${comentario.usuario}</h6>
+                        <span>${fechaActual}</span>
+                        
+                        <i class="fa fa-heart"></i>
+                    </div>
+                    <div class="comment-content">
+                        ${comentario.comentario}
+                    </div>
+                </div>
+            </div>
+            
+        `;
+        comentariosList.appendChild(nuevoComentario);
+    });
+}
 
+loadComments();
